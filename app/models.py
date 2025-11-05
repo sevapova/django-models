1a9df75e23de0f306491576e704434a8ba16b363
djumanov <djumanovdev@gmail.com>
2025-11-05 18:19:21 +0500

    done

--- app/models.py
+++ app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    PRIOTRY_CHOICES = [
+        ['1', 'Muxum'],
+        ['2', 'Sal Muxum'],
+        ['3', 'qilmasa ham boladi'],
+        ['4', 'asosiy'],
+    ]
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True, null=True, default="")
+    priority = models.CharField(
+        choices=PRIOTRY_CHOICES,
+        default='4'
+    )
+
+    def __repr__(self):
+        return f'Task(id={self.id}, name="{self.name}")'
+
+    def __str__(self):
+        return f'Task(id={self.id}, name="{self.name}")'
